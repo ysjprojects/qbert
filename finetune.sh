57201f00b2519dba5c9dde2bcf11b60992c13405
Yu Shi Jie <yshijie1999@gmail.com>
2025-03-28 17:10:57 -0400

    minor changes to finetune.sh

--- finetune.sh
+++ finetune.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-torchrun --nproc_per_node=4 finetune.py --batch_size 256 --model_path ./qbert-mlm-pretrain/checkpoint-23475 --from_scratch --qbert_mode only_attn
+torchrun --nproc_per_node=4 finetune.py --batch_size 256 --model_path ./qbert-mlm-pretrain/checkpoint-23475 --from_scratch
+wait
+torchrun --nproc_per_node=4 finetune.py --batch_size 256 --model_path ./qbert-mlm-pretrain/checkpoint-23475 --from_scratch --weighted_qk_concat
 wait
 torchrun --nproc_per_node=4 finetune.py --batch_size 256 --is_bert --model_path ./bert-mlm-pretrain/checkpoint-23475 --from_scratch
